97ff66a33e61720731d98d254260bccc9366d528
Manhattan Cafe <Neeted@users.noreply.github.com>
2026-02-23 14:37:51 +0900

    lang: add zh-CN, zh-TW, ko-KR and fix hardcoded culture check

--- BeMusicSeeker/Models/Localization/JsonBackedResourceManager.cs
+++ BeMusicSeeker/Models/Localization/JsonBackedResourceManager.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using System.Linq;
 using System.Reflection;
 using System.Resources;
 
@@ -6,33 +7,33 @@ namespace BeMusicSeeker.Models.Localization;
 
 public sealed class JsonBackedResourceManager : ResourceManager
 {
-	private static readonly CultureInfo JaCulture = CultureInfo.GetCultureInfo("ja-JP");
+    private static readonly CultureInfo JaCulture = CultureInfo.GetCultureInfo("ja-JP");
 
-	private readonly ResourceManager fallbackResourceManager;
+    private readonly ResourceManager fallbackResourceManager;
 
-	public JsonBackedResourceManager(string baseName, Assembly assembly)
-		: base(baseName, assembly)
-	{
-		fallbackResourceManager = new ResourceManager(baseName, assembly);
-	}
+    public JsonBackedResourceManager(string baseName, Assembly assembly)
+        : base(baseName, assembly)
+    {
+        fallbackResourceManager = new ResourceManager(baseName, assembly);
+    }
 
-	public override string GetString(string name, CultureInfo culture)
-	{
-		CultureInfo cultureInfo = culture ?? CultureInfo.CurrentUICulture;
-		string cultureName = cultureInfo.Name;
-		if (cultureName == "en-US" || cultureName == "fr-FR")
-		{
-			if (JsonLanguageCatalog.TryGetString(cultureName, name, out var value))
-			{
-				return value;
-			}
-			return fallbackResourceManager.GetString(name, JaCulture);
-		}
-		return fallbackResourceManager.GetString(name, cultureInfo);
-	}
+    public override string GetString(string name, CultureInfo culture)
+    {
+        CultureInfo cultureInfo = culture ?? CultureInfo.CurrentUICulture;
+        string cultureName = cultureInfo.Name;
+        if (cultureName != "ja-JP" && App.AvailableCultures.Values.Contains(cultureName))
+        {
+            if (JsonLanguageCatalog.TryGetString(cultureName, name, out var value))
+            {
+                return value;
+            }
+            return fallbackResourceManager.GetString(name, JaCulture);
+        }
+        return fallbackResourceManager.GetString(name, cultureInfo);
+    }
 
-	public override object GetObject(string name, CultureInfo culture)
-	{
-		return fallbackResourceManager.GetObject(name, culture ?? CultureInfo.CurrentUICulture);
-	}
+    public override object GetObject(string name, CultureInfo culture)
+    {
+        return fallbackResourceManager.GetObject(name, culture ?? CultureInfo.CurrentUICulture);
+    }
 }
