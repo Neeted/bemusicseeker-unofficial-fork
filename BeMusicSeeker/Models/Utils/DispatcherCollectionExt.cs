c60e92639ea81fc0c13de7573dd8eaa561c9fb5e
Manhattan Cafe <Neeted@users.noreply.github.com>
2026-02-25 23:51:12 +0900

    Fix NullReferenceException in DispatcherCollectionExt.RemoveExt by using RemoveAt

--- BeMusicSeeker/Models/Utils/DispatcherCollectionExt.cs
+++ BeMusicSeeker/Models/Utils/DispatcherCollectionExt.cs
@@ -6,40 +6,40 @@ namespace BeMusicSeeker.Models.Utils;
 
 internal static class DispatcherCollectionExt
 {
-	public static void AddRange<T>(this DispatcherCollection<T> collection, IEnumerable<T> items)
-	{
-		foreach (T item in items)
-		{
-			collection.Add(item);
-		}
-	}
+    public static void AddRange<T>(this DispatcherCollection<T> collection, IEnumerable<T> items)
+    {
+        foreach (T item in items)
+        {
+            collection.Add(item);
+        }
+    }
 
-	public static bool RemoveExt<T>(this DispatcherCollection<T> collection, T item) where T : class
-	{
-		int num = collection.IndexOf(item);
-		if (num != -1)
-		{
-			collection[num] = null;
-			return collection.Remove(null);
-		}
-		return false;
-	}
+    public static bool RemoveExt<T>(this DispatcherCollection<T> collection, T item) where T : class
+    {
+        int num = collection.IndexOf(item);
+        if (num != -1)
+        {
+            collection.RemoveAt(num);
+            return true;
+        }
+        return false;
+    }
 
-	public static List<T> Remove<T>(this DispatcherCollection<T> collection, IEnumerable<T> items) where T : class
-	{
-		List<T> list = new List<T>();
-		foreach (int item in Enumerable.Range(0, items.Count()))
-		{
-			if (collection.RemoveExt(items.ElementAt(item)))
-			{
-				list.Add(items.ElementAt(item));
-			}
-		}
-		return list;
-	}
+    public static List<T> Remove<T>(this DispatcherCollection<T> collection, IEnumerable<T> items) where T : class
+    {
+        List<T> list = new List<T>();
+        foreach (int item in Enumerable.Range(0, items.Count()))
+        {
+            if (collection.RemoveExt(items.ElementAt(item)))
+            {
+                list.Add(items.ElementAt(item));
+            }
+        }
+        return list;
+    }
 
-	public static void Replace<T>(this DispatcherCollection<T> collection, T src, T dst)
-	{
-		collection[collection.IndexOf(src)] = dst;
-	}
+    public static void Replace<T>(this DispatcherCollection<T> collection, T src, T dst)
+    {
+        collection[collection.IndexOf(src)] = dst;
+    }
 }
