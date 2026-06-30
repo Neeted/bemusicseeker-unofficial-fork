5b4f7f8a7e4baa472b660ad1aad9a9a59290725d
Manhattan Cafe <Neeted@users.noreply.github.com>
2026-06-30 23:03:21 +0900

    Bump version to 2.1.5.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Windows;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyInformationalVersion("2.1.4.0")]
+[assembly: AssemblyInformationalVersion("2.1.5.0")]
 [assembly: AssemblyVersion("0.1.7374.5230")]
 [assembly: InternalsVisibleTo("BeMusicSeeker.Tests")]
